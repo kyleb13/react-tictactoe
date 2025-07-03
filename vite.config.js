c6e66e781171893b8e8aca855433a4cdfea86e00
Kyle Beveridge <kylebeveridge13@gmail.com>
2025-07-03 12:07:32 -0700

    Reflattening project structure

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
-export default {
-  "base":'/react-tictactoe/'
-}
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/react-tictactoe/'
+})
